6592c7ae815a3f265f54879a28eeb219d7064092
Elvis <elvis.crego@gmail.com>
2019-12-22 23:09:18 -0500

    Agregando proyecto web

---
+++ PVRA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PVRA.Startup))]
+namespace PVRA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
